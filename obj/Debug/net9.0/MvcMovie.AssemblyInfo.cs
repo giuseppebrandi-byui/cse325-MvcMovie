3d95b23102c8e29214095d2d879c2c1cd44cda99
giuseppebrandi-byui <bra22041@byui.edu>
2025-09-09 08:13:30 +0100

    Change Views and Layout Pages

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3952cd1b2774208462b07001ecc9fc8335113e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44257334fc5f2544e910fe5f209059f45187563f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
