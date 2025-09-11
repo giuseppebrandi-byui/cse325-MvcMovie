a8a1d8355f2723afb2ad490ec06d7e878f903d54
giuseppebrandi-byui <bra22041@byui.edu>
2025-09-11 07:12:21 +0100

    Added rating field

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7bdc79f95b50c87ba23aa55d6f741a7d0d73056")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ccf5935f442e29ec8fe3eaa0d9000dddac6b61")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
