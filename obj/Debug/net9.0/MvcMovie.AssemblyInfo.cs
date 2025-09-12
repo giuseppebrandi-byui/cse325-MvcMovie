128f616c82861547f398dcc3464b4aba9ea7076d
giuseppebrandi-byui <bra22041@byui.edu>
2025-09-12 21:50:29 +0100

    Final Commitment

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ee5b574f8c2470ee7431772ddd7e3b06b5a7c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa78b6a5a851249beb57b6666c023b0c6a4430d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
